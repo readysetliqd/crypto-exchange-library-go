3739ecbfcf7c4c0dfe9c72189dfe15cd3a3906ee
readyset-liqd <135415819+readysetliqd@users.noreply.github.com>
2023-12-28 16:19:40 +0700

    added GetServerTime() function

--- pkg/kraken-spot/api.go
+++ pkg/kraken-spot/api.go
@@ -1,1 +1,38 @@
 package krakenspot
+
+import (
+	"crypto-exchange-library-go/pkg/kraken-spot/internal/data"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func GetServerTime() {
+	url := data.PublicApiUrl + "Time"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Println("error getting response from url | ", err, url)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusOK {
+		resp := data.ApiResp{}
+		msg, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Println("error calling io.readall | ", err)
+			return
+		}
+
+		err = json.Unmarshal(msg, &resp)
+		if err != nil {
+			log.Println("error unmarshaling msg to json | ", err)
+		}
+	} else {
+		log.Println("http status code not OK status code | ", res.StatusCode)
+	}
+}
+
+func GetSystemStatus() {
+	return
+}
