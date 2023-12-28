982ab6712a2e0c7ace874e6fa3256bc6214cdcf4
readyset-liqd <135415819+readysetliqd@users.noreply.github.com>
2023-12-28 20:39:55 +0700

    add function for calling Asset Info endpoint

--- pkg/kraken-spot/api.go
+++ pkg/kraken-spot/api.go
@@ -4,11 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/readysetliqd/crypto-exchange-library-go/pkg/kraken-spot/internal/data"
 )
 
+// Calls Kraken API public market data "Time" endpoint. Gets the server's time.
+// data.ServerTime struct
 func GetServerTime() (*data.ServerTime, error) {
 	serverTime := &data.ServerTime{}
 	err := callPublicApi("Time", serverTime)
@@ -18,6 +21,8 @@ func GetServerTime() (*data.ServerTime, error) {
 	return serverTime, nil
 }
 
+// Calls Kraken API public market data "SystemStatus" endpoint. Gets the current
+// system status or trading mode
 func GetSystemStatus() (*data.SystemStatus, error) {
 	systemStatus := &data.SystemStatus{}
 	err := callPublicApi("SystemStatus", systemStatus)
@@ -27,6 +32,33 @@ func GetSystemStatus() (*data.SystemStatus, error) {
 	return systemStatus, nil
 }
 
+// Calls Kraken API public market data "SystemStatus" endpoint and returns true
+// if system is online. Returns false and current status as a string if not
+// online. Returns false and error if error produced from calling API.
+func SystemIsOnline() (bool, string) {
+	systemStatus, err := GetSystemStatus()
+	if err != nil {
+		log.Println("error calling GetSystemStatus() | ", err)
+		return false, "error"
+	}
+	if systemStatus.Status == "online" {
+		return true, systemStatus.Status
+	}
+	return false, systemStatus.Status
+}
+
+func GetAllAssetInfo() (*map[string]data.AssetInfoAsset, error) {
+	allAssetInfo := &map[string]data.AssetInfoAsset{}
+	err := callPublicApi("Assets", allAssetInfo)
+	if err != nil {
+		return nil, err
+	}
+	return allAssetInfo, nil
+}
+
+// Calls Kraken's public api endpoint. Args endpoint string should match the url
+// endpoint from the api docs. Args target interface{} should be a pointer to
+// an empty struct of the matching endpoint data type
 func callPublicApi(endpoint string, target interface{}) error {
 	url := data.PublicApiUrl + endpoint
 	res, err := http.Get(url)
@@ -37,19 +69,24 @@ func callPublicApi(endpoint string, target interface{}) error {
 
 	if res.StatusCode == http.StatusOK {
 		resp := data.ApiResp{Result: target}
+		respMap := map[string]interface{}{}
 		msg, err := io.ReadAll(res.Body)
 		if err != nil {
 			return fmt.Errorf("error calling io.readall | %v", err)
 		}
-
 		err = json.Unmarshal(msg, &resp)
 		if err != nil {
-			return fmt.Errorf("error unmarshaling msg to json | %v", err)
+			return fmt.Errorf("error unmarshaling msg to ApiResp | %v", err)
+		}
+		err = json.Unmarshal(msg, &respMap)
+		if err != nil {
+			return fmt.Errorf("error unmarshaling msg to map | %v", err)
+		}
+		if _, ok := respMap["result"]; !ok {
+			return fmt.Errorf("api error | no \"result\" field")
 		}
 		if len(resp.Error) != 0 {
 			return fmt.Errorf("api error(s) | %v", resp.Error)
-		} else if resp.Result == nil {
-			return fmt.Errorf("api error | no \"result\" field")
 		}
 	} else {
 		return fmt.Errorf("http status code not OK status code | %v", res.StatusCode)
--- pkg/kraken-spot/internal/data/data.go
+++ pkg/kraken-spot/internal/data/data.go
@@ -1,15 +1,29 @@
 package data
 
+type ApiResp struct {
+	Error  []string    `json:"error"`
+	Result interface{} `json:"result"`
+}
+
 type ServerTime struct {
 	UnixTime int    `json:"unixtime"`
 	Rfc1123  string `json:"rfc1123"`
 }
+
 type SystemStatus struct {
 	Status    string `json:"status"`
 	Timestamp string `json:"timestamp"`
 }
 
-type ApiResp struct {
-	Error  []string    `json:"error"`
-	Result interface{} `json:"result"`
+type AssetInfo struct {
+	Assets []AssetInfoAsset
+}
+
+type AssetInfoAsset struct {
+	Aclass          string
+	Altname         string
+	Decimals        uint8
+	DisplayDecimals uint8
+	CollateralValue float32
+	Status          string
 }
