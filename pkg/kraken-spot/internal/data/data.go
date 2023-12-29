d8e81c8bad022aff10a5f8f9304b1cc45320dfc3
readyset-liqd <135415819+readysetliqd@users.noreply.github.com>
2023-12-29 15:11:17 +0700

    fix fees data type to array of arrays

--- pkg/kraken-spot/internal/data/data.go
+++ pkg/kraken-spot/internal/data/data.go
@@ -25,32 +25,27 @@ type AssetInfo struct {
 }
 
 type AssetPair struct {
-	Altname            string  `json:"altname"`
-	Wsname             string  `json:"wsname"`
-	AclassBase         string  `json:"aclass_base"`
-	Base               string  `json:"base"`
-	AclassQuote        string  `json:"aclass_quote"`
-	Quote              string  `json:"quote"`
-	PairDecimals       uint8   `json:"pair_decimals"`
-	CostDecimals       uint8   `json:"cost_decimals"`
-	LotDecimals        uint8   `json:"lot_decimals"`
-	LotMultiplier      uint8   `json:"lot_multiplier"`
-	LeverageBuy        []uint8 `json:"leverage_buy"`
-	LeverageSell       []uint8 `json:"leverage_sell"`
-	Fees               []Fee   `json:"fees"`
-	FeesMaker          []Fee   `json:"fees_maker"`
-	FeeVolumeCurrency  string  `json:"fee_volume_currency"`
-	MarginCall         uint8   `json:"margin_call"`
-	MarginStop         uint8   `json:"margin_stop"`
-	OrderMin           string  `json:"ordermin"`
-	CostMin            string  `json:"costmin"`
-	TickSize           string  `json:"tick_size"`
-	Status             string  `json:"status"`
-	LongPositionLimit  uint32  `json:"long_position_limit"`
-	ShortPositionLimit uint32  `json:"short_position_limit"`
-}
-
-type Fee struct {
-	Threshold  uint32
-	FeePercent float32
+	Altname            string      `json:"altname"`
+	Wsname             string      `json:"wsname"`
+	AclassBase         string      `json:"aclass_base"`
+	Base               string      `json:"base"`
+	AclassQuote        string      `json:"aclass_quote"`
+	Quote              string      `json:"quote"`
+	PairDecimals       uint8       `json:"pair_decimals"`
+	CostDecimals       uint8       `json:"cost_decimals"`
+	LotDecimals        uint8       `json:"lot_decimals"`
+	LotMultiplier      uint8       `json:"lot_multiplier"`
+	LeverageBuy        []uint8     `json:"leverage_buy"`
+	LeverageSell       []uint8     `json:"leverage_sell"`
+	Fees               [][]float64 `json:"fees"`
+	FeesMaker          [][]float64 `json:"fees_maker"`
+	FeeVolumeCurrency  string      `json:"fee_volume_currency"`
+	MarginCall         uint8       `json:"margin_call"`
+	MarginStop         uint8       `json:"margin_stop"`
+	OrderMin           string      `json:"ordermin"`
+	CostMin            string      `json:"costmin"`
+	TickSize           string      `json:"tick_size"`
+	Status             string      `json:"status"`
+	LongPositionLimit  uint32      `json:"long_position_limit"`
+	ShortPositionLimit uint32      `json:"short_position_limit"`
 }
